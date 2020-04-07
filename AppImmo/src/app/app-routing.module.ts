6e7af67fd01e43bac41c47a85a258cf6e3629542
PerrineLer <perrine.leroy76@gmail.com>
2020-04-07 17:47:44 +0200

    Creation de la nav bar et debut du formulaire proprietaire

--- AppImmo/src/app/app-routing.module.ts
+++ AppImmo/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProprietairesComponent } from './proprietaires/proprietaires.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'proprietaires', component:ProprietairesComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
