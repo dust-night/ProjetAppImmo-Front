9e3350969ab0532f80234b776d8e51cd7805cc9c
dust-night <aimi-manami@hotmail.fr>
2020-04-07 17:47:40 +0200

    Création du formulaire Client

--- AppImmo/src/app/app.module.ts
+++ AppImmo/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { ClientFormComponent } from './client/client-form/client-form.component';
+import { ClientListComponent } from './client/client-list/client-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ClientFormComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
