10533bff146bd95f10a234c459cc036edc36d67b
dust-night <aimi-manami@hotmail.fr>
2020-04-08 18:40:56 +0200

    modification et avancé du fichier client mais il y a a encore des erreur dessus

--- AppImmo/src/app/app-routing.module.ts
+++ AppImmo/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { ClientFormComponent } from './client/client-form/client-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'client', component: ClientFormComponent,children: [
+{path: 'edit/:id', component:ClientFormComponent}
+
+
+
+]
+}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AppImmo/src/app/client/client-form/client-form.component.ts
+++ AppImmo/src/app/client/client-form/client-form.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
   styleUrls: ['./client-form.component.css']
 })
+
+
+
 export class ClientFormComponent implements OnInit {
+  form: FormGroup;
+  id:any;
+  mode: boolean;
+  client: any;
+  clients: any[];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.form = new FormGroup({
+      id: new FormControl(null, Validators.required),
+      nom: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+      prenom: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+      
+  });
+
+}
+
+
+
+addClient(){
+
+
+
+}
+
+
+
 
 }
