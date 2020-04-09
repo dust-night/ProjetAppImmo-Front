ea0b7885a417ae5215351c71321d6317fceec180
PerrineLer <perrine.leroy76@gmail.com>
2020-04-09 18:35:11 +0200

    fin de la nav barre et du formulaire proprietaire. Commencement de la page d'accueil

--- AppImmo/src/app/app-routing.module.ts
+++ AppImmo/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProprietairesComponent } from './proprietaires/proprietaires.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 const routes: Routes = [
-{path:'proprietaires', component:ProprietairesComponent}
+{path:'proprietaires', component:ProprietairesComponent},
+{path:'accueil', component:AccueilComponent}
 
 
 ];
--- AppImmo/src/app/app.module.ts
+++ AppImmo/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProprietairesComponent } from './proprietaires/proprietaires.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ProprietaireListComponent } from './proprietaire-list/proprietaire-list.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProprietairesComponent
+    ProprietairesComponent,
+    ProprietaireListComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+   
+    FormsModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
