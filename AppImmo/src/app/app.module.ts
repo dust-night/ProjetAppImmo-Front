e68f42ff3cb33d92a7062741efc3c3ba19479947
PerrineLer <perrine.leroy76@gmail.com>
2020-04-10 16:30:48 +0200

    ajout sur la branch master

--- AppImmo/src/app/app.module.ts
+++ AppImmo/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProprietairesComponent } from './proprietaires/proprietaires.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ProprietaireListComponent } from './proprietaire-list/proprietaire-list.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -19,8 +23,12 @@ import { AccueilComponent } from './accueil/accueil.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    NgbModalModule,
+    FlatpickrModule,
+    CommonModule,
+    FormsModule,
    
-    FormsModule
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
     
     
   ],
