bcf90dd160f6a7af5a8b01c943fa9b8381b66a0c
dust-night <aimi-manami@hotmail.fr>
2020-04-12 15:45:05 +0200

    Ajout d'une classe bien immobiliers, d'une liste et construction d'un formulaire

--- AppImmo/src/app/bienimmob/bienimmob.component.ts
+++ AppImmo/src/app/bienimmob/bienimmob.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-bienimmob',
   templateUrl: './bienimmob.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienimmobComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  redirect(){
+    
+    this.router.navigate(['bienimmob/tableauimmo'])
+
+}
+
+rediriger(){
+
+  this.router.navigate(['bienimmob/immoform'])
+}
+
 
 }
---
+++ AppImmo/src/app/bienimmob/immob-form/immob-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immob-form',
+  templateUrl: './immob-form.component.html',
+  styleUrls: ['./immob-form.component.css']
+})
+export class ImmobFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
