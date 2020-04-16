271f76dde2ff9281e18e016f4527b1ea68f69c60
DocKoneo <koneochester@gmail.com>
2020-04-16 12:07:29 +0200

    push master avec login

--- AppImmo/src/app/login/login.component.ts
+++ AppImmo/src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
 ngOnInit() {
     this.loginForm = this.formBuilder.group({
         username: ['', Validators.required],
-        password: ['', Validators.required]
+        password: ['', Validators.required] 
     });
 
     // reset login status
